882d0f9b6a90a8aa0de5512a6a0fcc13834b6c0a
Tushar Kant Nag <82863403+tushar-1202@users.noreply.github.com>
2023-09-01 09:59:25 +0530

    test.py

--- test.py
+++ test.py
@@ -2,3 +2,17 @@ print("testing v1")
 print("testing v2")
 print("testing v3")
 print("testing v3.1.1")
+rows = 8
+
+k = 0
+
+for i in range(1, rows+1):
+    for space in range(1, (rows-i)+1):
+        print(end="  ")
+   
+    while k!=(2*i-1):
+        print("* ", end="")
+        k += 1
+   
+    k = 0
+    print()
